83b71f0ab9563d8be8b4d0bd7189ef589a21f389
Alejandro <samuel@qordev.com>
2022-06-13 17:03:56 -0600

    Product Detail wodking

--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -36,6 +36,46 @@ class ProductDetail extends StatelessWidget {
               Hero(
                   tag: product.productId,
                   child: Image.network(product.productIamgeUrl)),
+              Positioned(
+                left: width * 0.83,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  color: Colors.black45,
+                  height: (height / 2) + 200,
+                  width: width * 0.18,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pushReplacementNamed('/');
+                        },
+                        icon: const Icon(
+                          Icons.home_rounded,
+                          size: 30,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.share_rounded,
+                          size: 30,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.favorite_outline_rounded,
+                          size: 30,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 220,
+                      )
+                    ],
+                  ),
+                ),
+              ),
               Positioned.fill(
                 top: (height / 2) - (height * 0.18),
                 child: ClipPath(
@@ -223,133 +263,17 @@ class ProductDetail extends StatelessWidget {
                   clipper: ProductDetailClipper(),
                 ),
               ),
-              // Positioned(
-              //   top: (height / 2),
-              //   height: (height / 2) + 40,
-              //   child: Column(
-              //     mainAxisSize: MainAxisSize.max,
-              //     crossAxisAlignment: CrossAxisAlignment.start,
-              //     children: [
-              //       Container(
-              //         padding: const EdgeInsets.only(left: 20),
-              //         width: width,
-              //         child: Column(
-              //           mainAxisSize: MainAxisSize.max,
-              //           crossAxisAlignment: CrossAxisAlignment.start,
-              //           children: [
-              //             Text(
-              //               product.productName,
-              //               style: TextStyle(
-              //                   fontWeight: FontWeight.w600,
-              //                   fontSize: 26,
-              //                   color:
-              //                       Theme.of(context).colorScheme.onBackground),
-              //             ),
-              //             const SizedBox(
-              //               height: 10,
-              //             ),
-              //             Row(
-              //               children: [
-              //                 Text(
-              //                   'Designer: ',
-              //                   style:
-              //                       Theme.of(context).textTheme.headlineMedium,
-              //                 ),
-              //                 Text(product.designer,
-              //                     style: Theme.of(context).textTheme.bodyMedium)
-              //               ],
-              //             ),
-              //             const SizedBox(
-              //               height: 10,
-              //             ),
-              //             Row(
-              //               children: [
-              //                 Text('Materials: ',
-              //                     style: Theme.of(context)
-              //                         .textTheme
-              //                         .headlineMedium),
-              //                 Text(product.materials,
-              //                     style: Theme.of(context).textTheme.bodyMedium)
-              //               ],
-              //             ),
-              //             const SizedBox(
-              //               height: 20,
-              //             ),
-              //             Text(
-              //               '€${product.productPrice}',
-              //               style: GoogleFonts.mulish(
-              //                 textStyle: TextStyle(
-              //                     fontWeight: FontWeight.w800,
-              //                     fontSize: 22,
-              //                     color: Theme.of(context).colorScheme.primary),
-              //               ),
-              //             ),
-              //             const SizedBox(
-              //               height: 20,
-              //             ),
-              //           ],
-              //         ),
-              //       ),
-              //       Container(
-              //         padding: EdgeInsets.symmetric(horizontal: 5),
-              //         width: width,
-              //         child: Row(
-              //           mainAxisSize: MainAxisSize.max,
-              //           mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //           children: [
-              //             DescriptionCard(product.features.height),
-              //             DescriptionCard(product.features.width),
-              //             DescriptionCard(product.features.diagonal),
-              //             DescriptionCard(product.features.potency)
-              //           ],
-              //         ),
-              //       ),
-              //       Expanded(child: Container()),
-              //       SizedBox(
-              //         width: width,
-              //         child: Row(
-              //           children: [
-              //             Container(
-              //               padding: const EdgeInsets.only(
-              //                   left: 50, top: 10, bottom: 25, right: 60),
-              //               child: SvgPicture.asset(
-              //                 'assets/icons/bag_icon.svg',
-              //                 width: 32,
-              //                 height: 32,
-              //                 color: Theme.of(context).colorScheme.primary,
-              //               ),
-              //             ),
-              //             Expanded(
-              //               child: InkWell(
-              //                 onTap: () {
-              //                   log('This is going to log');
-              //                 },
-              //                 child: Container(
-              //                   child: Center(
-              //                     child: Text(
-              //                       'Buy Now',
-              //                       style: Theme.of(context)
-              //                           .textTheme
-              //                           .headlineLarge!
-              //                           .merge(const TextStyle(
-              //                               color: Colors.white)),
-              //                     ),
-              //                   ),
-              //                   height: 70,
-              //                   decoration: BoxDecoration(
-              //                       borderRadius: const BorderRadius.only(
-              //                           topLeft: Radius.circular(40)),
-              //                       color:
-              //                           Theme.of(context).colorScheme.primary),
-              //                 ),
-              //               ),
-              //             )
-              //           ],
-              //         ),
-              //       )
-              //     ],
-              //   ),
-              // )
+              Positioned(
+                left: 15,
+                top: 20,
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back_ios),
+                  color: Colors.white,
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
             ],
           ),
         ),
